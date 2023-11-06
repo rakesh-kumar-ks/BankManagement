08b23ac12a7fff0bb2df85edd09ad748e623c934
rakesh-kumar-ks <yash04rakesh@gmail.com>
2023-11-06 17:57:08 +0530

    only backend code

--- frontend/bankmanagement/src/components/errormessage/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import {Alert} from 'react-bootstrap';
-
-export default function ErrorMessage({variant,children}:any) {
-  return (
-    <Alert variant={variant} style={{fontSize:15}}>
-        <strong>{children}</strong>
-    </Alert>
-  )
-}
